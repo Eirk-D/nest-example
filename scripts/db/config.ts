b45414197127c303065b6634869f038abf2dcaa8
Derik <89432483a@gmail.com>
2021-12-07 01:09:13 +0800

    change

--- scripts/db/config.ts
+++ scripts/db/config.ts
@@ -1,6 +1,7 @@
 import { Todo } from '../../src/modules/resources/todos/entities/todo.entity';
 import { User } from '../../src/modules/resources/users/entities/user.entity';
 import { ConnectionOptions } from 'typeorm/connection/ConnectionOptions';
+import { City } from '../../src/modules/resources/cities/entities/city.entity';
 
 const ormConfig: ConnectionOptions = {
   type: 'mariadb',
@@ -9,7 +10,7 @@ const ormConfig: ConnectionOptions = {
   port: 3306,
   username: 'root',
   password: '123456789',
-  entities: [User, Todo],
+  entities: [User, Todo, City],
 };
 
 export default ormConfig;
--- src/modules/resources/cities/entities/city.entity.ts
+++ src/modules/resources/cities/entities/city.entity.ts
@@ -1,1 +1,13 @@
-export class City {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class City {
+  @ApiProperty({ description: '自增id' })
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty({ description: '名称' })
+  @Column({ length: 200 })
+  name: string;
+}
